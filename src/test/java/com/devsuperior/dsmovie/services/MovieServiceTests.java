4bc55525021e36bb5a94bb0e418ef098192e9c21
Lucas Muniz <lucasmunizfk@gmail.com>
2025-04-24 18:12:43 -0300

    test: findAll movie unit test

--- src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
@@ -1,14 +1,29 @@
 package com.devsuperior.dsmovie.services;
 
+import static org.mockito.ArgumentMatchers.any;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
+import org.mockito.Mockito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.entities.MovieEntity;
 import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.tests.MovieFactory;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @ExtendWith(SpringExtension.class)
 public class MovieServiceTests {
@@ -18,10 +33,40 @@ public class MovieServiceTests {
 	
 	@Mock
 	private MovieRepository repository;
+	
+	private PageImpl<MovieEntity> page;
+	private Long existingId;
+	private Long nonExistingId;
+	private MovieEntity movie;
+	private MovieDTO movieDTO;
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		existingId = 1L;
+		nonExistingId = 2L;
+		
+		movie = MovieFactory.createMovieEntity();
+		movieDTO = MovieFactory.createMovieDTO();
+		page = new PageImpl<>(List.of(movie));
+		
+		Mockito.when(repository.searchByTitle(any(), (Pageable)any())).thenReturn(page);
+		Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(movie));
+		Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
+		Mockito.when(repository.save(any())).thenReturn(movie);
+		Mockito.when(repository.getReferenceById(existingId)).thenReturn(movie);
+		Mockito.when(repository.getReferenceById(existingId)).thenThrow(EntityNotFoundException.class);
+	}
+
 
 	@Test
 	public void findAllShouldReturnPagedMovieDTO() {
+		PageRequest pageRequest = PageRequest.of(0, 12);
+		
+		Page<MovieDTO> result = service.findAll("", pageRequest);
+		
+		Assertions.assertNotNull(result);
 	}
+
 	
 	@Test
 	public void findByIdShouldReturnMovieDTOWhenIdExists() {
