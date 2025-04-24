c728de2d4c7ea0678d6dd4cd90da4de518115ec9
Lucas Muniz <lucasmunizfk@gmail.com>
2025-04-24 18:42:17 -0300

    authenticated method UserService unit tests

--- src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
@@ -1,14 +1,24 @@
 package com.devsuperior.dsmovie.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.devsuperior.dsmovie.entities.UserEntity;
 import com.devsuperior.dsmovie.repositories.UserRepository;
+import com.devsuperior.dsmovie.tests.UserFactory;
 import com.devsuperior.dsmovie.utils.CustomUserUtil;
 
 @ExtendWith(SpringExtension.class)
@@ -26,18 +36,35 @@ public class UserServiceTests {
 
     private String validUsername;
     private String invalidUsername;
+    private UserEntity userValid;
     
     @BeforeEach
     void setUp() {
+    	validUsername = "Maria";
+    	invalidUsername = "Joao";
+    	userValid = UserFactory.createUserEntity();
     	
+    	Mockito.when(repository.findByUsername(validUsername)).thenReturn(Optional.of(userValid));
+    	Mockito.when(repository.findByUsername(invalidUsername)).thenReturn(Optional.empty());
 	}
     
 	@Test
 	public void authenticatedShouldReturnUserEntityWhenUserExists() {
+		Mockito.when(userUtil.getLoggedUsername()).thenReturn(validUsername);
+		UserEntity result = service.authenticated();
+		
+		assertNotNull(result);
+		assertEquals(userValid.getName(), result.getName());
 	}
 
 	@Test
 	public void authenticatedShouldThrowUsernameNotFoundExceptionWhenUserDoesNotExists() {
+		Mockito.when(userUtil.getLoggedUsername()).thenReturn(invalidUsername);
+		assertThrows(UsernameNotFoundException.class, () -> {
+			service.authenticated();
+		});
+		Mockito.verify(userUtil, Mockito.times(1)).getLoggedUsername();
+		Mockito.verify(repository, Mockito.times(1)).findByUsername(invalidUsername);
 	}
 
 	@Test
